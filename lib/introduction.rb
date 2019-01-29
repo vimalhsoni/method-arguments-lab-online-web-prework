979729527f311078168f581d4b96d3a73b042871
Vimal Soni <vimal.h.soni@gmail.com>
2019-01-29 04:09:42 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def ntroduction_with_language
+def '#introduction_with_language'
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
